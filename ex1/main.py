129dad9634956c3c8ec7869147aa069dfc985e7e
Artyom <utyagulov05@gmail.com>
2024-03-03 12:12:08 +0300

    second one is unsolvable

--- ex1/main.py
+++ ex1/main.py
@@ -1,4 +1,4 @@
-import math
+from math import cos, radians
 
 print(f"Введите через запятую с пробелом все данные h, S1, S2, a")
 all_in = input()
@@ -10,7 +10,7 @@ a = int(a)
 
 V = h/3 * (S1 + (S1*S2)**0.5 + S2)
 
-SB = (S1 - S2) / math.cos(math.radians(a))
+SB = (S1 - S2) / cos(radians(a))
 SB = abs(SB)
 
-print(V, SB)
+print("{x:.2f}".format(x = V), "{x:.2f}".format(x = SB))
--- ex2/main.py
+++ ex2/main.py
@@ -1,6 +1,26 @@
-a = int(input("Введите A \n"))
+import math
 
-z1 = (((1+a+a*a)/(2*a+a*a))+2-((1-a+a*a)/(2*a-a*a)))*(5-2*a*a)
-z2 = (4-a*a)/2
+#a = float(input("Введите A \n"))
+#a = math.radians(a)
 
-print(z1, z2)
+
+def functiona(a):
+ step0:float = ((((((1 + a) + (round(math.pow(a,2),5))) / ((2 * a) + (round(math.pow(a,2),5)))) + 2) - (((1 - a) + (round(math.pow(a,2),5))) / ((2 * a) - (round(math.pow(a,2),5))))) * (5 - (2 * (round(math.pow(a,2),5)))))
+ return step0
+
+
+def functionb(a):
+ step0:float = ((4 - (round(math.pow(a,2),5))) / 2)
+ return step0
+#z1 = (((1+a+a*a)/(2*a+a*a))+2-((1-a+a*a)/(2*a-a*a)))*(5-2*a*a)
+#z2 = (4-a*a)/2
+
+for a in range(10, 3601):
+ a = a / 10
+ a = math.radians(a)
+ z1 = functiona(a)
+ z2 = functionb(a)
+ z1 = "{x:.2f}".format(x = z1)
+ z2 = "{x:.2f}".format(x = z2)
+ if z1 == z2:
+    print(z1, z2, a)
